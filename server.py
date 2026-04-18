e5037968abb0a5c6215e8a325511cf45b768da83
ashurovm090-wq <ashurovm090@gmail.com>
2026-04-18 20:42:06 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 import os
+import json
 
 app = FastAPI()
 
@@ -26,14 +27,18 @@ def serve_html(file_name: str):
 
 @app.get("/")
 async def index(): return serve_html("index.html")
+
+@app.get("/chats")
+async def chats_list(): return serve_html("chats.html")
+
 @app.get("/chat")
 async def chat(): return serve_html("chat.html")
+
 @app.get("/profile")
 async def profile(): return serve_html("profile.html")
+
 @app.get("/edit")
 async def edit(): return serve_html("edit.html")
-@app.get("/search")
-async def search(): return serve_html("search.html")
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
